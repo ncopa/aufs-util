e391c8d6e37f4fe09b737598fba86638d70b4482
J. R. Okajima <hooanon05g@gmail.com>
2014-07-26 11:06:51 +0900

    support for musl libc 1/2, define _GNU_SOURCE in Makefile
    For next commit to support for musl libc, every source file needs to
    define _GNU_SOURCE. So do it in Makefile.
    
    Reported-by: Sean Lynch <seanl@literati.org>
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- proc_mnt.c
+++ proc_mnt.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2011 Junjiro R. Okajima
+ * Copyright (C) 2005-2014 Junjiro R. Okajima
  *
  * This program, aufs is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#define _GNU_SOURCE /* getmntent_r */
 #include <sys/types.h>
 #include <errno.h>
 #include <mntent.h>
