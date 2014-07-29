ee3a4090e5cd7cfd9ce4ecad28eb9497f0f96717
J. R. Okajima <hooanon05g@gmail.com>
2014-07-29 13:49:24 +0900

    remove forgotten several _GNU_SOURCE macro
    The macro is already defined in Makefile, and the definition in source
    files may produce a warning.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- libau/rdu.c
+++ libau/rdu.c
@@ -16,8 +16,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#define _GNU_SOURCE
-
 #include <sys/vfs.h>    /* or <sys/statfs.h> */
 #include <errno.h>
 #include <stdio.h>
