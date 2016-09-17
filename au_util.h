1ca264d941f2380a6f6cb591d6318aee3b09b1ed
J. R. Okajima <hooanon05g@gmail.com>
2016-09-18 00:31:02 +0900

    aufs-util 20160919
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- au_util.h
+++ au_util.h
@@ -33,7 +33,7 @@ static inline char *au_decode_mntpnt(char *src, char *dst, int len)
 char *au_decode_mntpnt(char *src, char *dst, int len);
 #endif
 
-#define AuRelease	"20160905"
+#define AuRelease	"20160919"
 #ifdef AUFHSM
 #define AuFhsmStr " with FHSM"
 #else
